bdb5651f0f714be72837e9d1f691917646ddff99
Eshan Chathuranga <eshanchathurangafdo@gmail.com>
2024-10-03 03:52:23 +0530

    ## Fix file path

--- SkyChat/IndexForm.cs
+++ SkyChat/IndexForm.cs
@@ -10,13 +10,16 @@ using System.Windows.Forms;
 using SkyChat.Lib.Components;
 using SkyChat.Lib.Components.IndexForm;
 using Newtonsoft.Json;
+using SkyChat.Lib.Modules;
 
 namespace SkyChat
 {
     public partial class IndexForm : Form
     {
+        // Define a object og Get File Path
+        GetFilePath getFilePath;
         // Define a AuthConfig File Path
-        private string AuthConfigPath = @"C:\\Users\\User\\Desktop\\Last_Project\\SkyChat\\SkyChat\\Lib\\Config\\AuthConfig.json";
+        private string AuthConfigPath;
         // Create a object of AuthConfig
         AuthConfig authConfig;
         public IndexForm()
@@ -25,27 +28,41 @@ namespace SkyChat
             // Read the AuthConfig file
             string json = System.IO.File.ReadAllText(AuthConfigPath);
             authConfig = JsonConvert.DeserializeObject<AuthConfig>(json);
+
+                InitializeComponent();
+            // Create a object og get file path
+            this.getFilePath = new GetFilePath();
+            // Get File path
+            this.AuthConfigPath = getFilePath.GetConfigFilePath("AuthConfig.json");
             // Check already login or not
-            //if (authConfig._id != null)
-            //{
-            //    MainForm mainForm = new MainForm();
-            //    mainForm.Show();
-            //    Task.Run(() =>
-            //    {
-            //        this.Invoke((MethodInvoker)delegate
-            //        {
-            //            this.Hide();
-            //        });
-            //    });
-            //}
-            //else
-            //{
+            if (authConfig._id != null)
+            {
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+                Task.Run(() =>
+                {
+                    if (this.InvokeRequired)
+                    {
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            this.Hide();
+                        });
+                    }
+                    else
+                    {
+                        this.Hide();
+                    }
+                });
+            }
+            else
+            {
                 InitializeComponent();
                 // Set the default user control to the index page
                 Login_Account_Comp login_Account_Comp = new Login_Account_Comp();
-                 login_Account_Comp.Location = new Point(0, 0);
-                 panelIndexContainer.Controls.Add(login_Account_Comp);
-            //}
+                login_Account_Comp.Location = new Point(0, 0);
+                panelIndexContainer.Controls.Add(login_Account_Comp);
+            }
+
         }
         private void IndexForm_Load_1(object sender, EventArgs e)
         {
